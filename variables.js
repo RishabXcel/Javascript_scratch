89ba0d62fab38580971954591928a9be81b925cc
RishabXcel <159923681+RishabXcel@users.noreply.github.com>
2024-12-03 11:20:33 +0545

    variables

---
+++ variables.js
@@ -0,0 +1,21 @@
+const accountId = 1234
+let accountEmail ="rishabpokharel123@gmail.com"
+var accountPassword ="12345"
+let accountCity = "Butwal"
+
+//accountId = 2 // not allowed
+
+accountEmail = "ri@ri.com"
+accountPassword = "1234"
+accountCity = "Butwal"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table[accountId, accountEmail, accountPassword, accountPassword]
+
+
