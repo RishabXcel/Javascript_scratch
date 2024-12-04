d138cb0569642a32386d257d29f64dfb72fe2037
RishabXcel <159923681+RishabXcel@users.noreply.github.com>
2024-12-04 15:56:20 +0545

    datatypes_continnues

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) //we are using nodejs, not browser
+
+let name = "rishab"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53 or 54
+// bigint - used in case of large number 
+// string => "" / '' prefered to use = ""
+// boolean => true of false -only 2 values
+// null => standalone value
+// undefined => let state; // no value included
+// symbol +.
